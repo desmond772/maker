a2f46b52e1a619fce6852c73bdcf4cd78e1314d6
desmond772 <ngwad896@gmail.com>
2025-09-02 19:59:21 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import FloatingBar from './FloatingBar';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Trading Automation Tool</h1>
+      <FloatingBar />
+      <div className="android-hint">
+        <span>Optimized for Android • Human-like bot</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
